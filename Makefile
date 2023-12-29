920660e2854a83838b7b563be605a630ff9e0ded
StiliyanKushev <stiliyankushev@gmail.com>
2023-12-29 04:29:54 +0200

    feat: basic native addon boilerplate

---
+++ Makefile
@@ -0,0 +1,3 @@
+# makes a .node file using dotnet 8 aot
+node:
+	dotnet publish dotnet-src -r linux-x64 -c Release
---
+++ dotnet-src/Addon.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.JavaScript.NodeApi;
+
+namespace AddonLibrary;
+
+[JSExport]
+public static class Addon
+{
+    [JSExport("printTestMessage")]
+    public static int PrintTestMessage() {
+        Console.WriteLine("PrintTestMessage(): Hello World.");
+        return 4;
+    }
+}
